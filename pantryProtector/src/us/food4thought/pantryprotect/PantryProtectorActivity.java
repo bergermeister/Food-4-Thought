4b83eb092fb4034989fbd944877a8bd17063b016
Nick Iaconis <iaconn@rpi.edu>
2011-09-18 20:46:29 -0400

    Empty Android 2.2 Project
    Change-Id: Ie8584c4f74fc4978109b51213158f88f834bd768

---
+++ pantryProtector/src/us/food4thought/pantryprotect/PantryProtectorActivity.java
@@ -0,0 +1,13 @@
+package us.food4thought.pantryprotect;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PantryProtectorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
