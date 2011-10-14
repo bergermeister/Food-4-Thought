5cb01806e531598f1b8850fca1c8b45ad117f11d
Ed <Ed@128.113.247.201>
2011-10-14 18:14:41 -0400

    Merged Scanner and began Notification merge

---
+++ pantryProtector/src/us/food4thought/pantryprotect/NotificationService.java
@@ -0,0 +1,84 @@
+package us.food4thought.pantryprotect;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class NotificationService extends Service {
+    private NotificationManager mNM;
+
+    /**
+     * Class for clients to access.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with
+     * IPC.
+     */
+    public class LocalBinder extends Binder {
+        NotificationService getService() {
+            return NotificationService.this;
+        }
+    }
+    
+    @Override
+    public void onCreate() {
+        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        // Display a notification about us starting.  We put an icon in the status bar.
+        showNotification();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("LocalService", "Received start id " + startId + ": " + intent);
+        // We want this service to continue running until it is explicitly
+        // stopped, so return sticky.
+        System.out.println("Service started, returning Sticky");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        // Cancel the persistent notification.
+        mNM.cancel(R.string.local_service_started);
+
+        // Tell the user we stopped.
+        Toast.makeText(this, R.string.local_service_stopped, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    // This is the object that receives interactions from clients.  See
+    // RemoteService for a more complete example.
+    private final IBinder mBinder = new LocalBinder();
+
+    /**
+     * Show a notification while this service is running.
+     */
+    private void showNotification() {
+        // In this sample, we'll use the same text for the ticker and the expanded notification
+        CharSequence text = getText(R.string.local_service_started);
+
+        // Set the icon, scrolling text and timestamp
+        Notification notification = new Notification(R.drawable.ic_tab_inv_g, text,
+                System.currentTimeMillis());
+
+        // The PendingIntent to launch our activity if the user selects this notification
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, PantryProtectorActivity.class), 0);
+
+        // Set the info for the views that show in the notification panel.
+        notification.setLatestEventInfo(this, getText(R.string.local_service_label),
+                       text, contentIntent);
+
+        // Send the notification.
+        // We use a layout id because it is a unique number.  We use it later to cancel.
+        mNM.notify(R.string.local_service_started, notification);
+    }
+}
--- pantryProtector/src/us/food4thought/pantryprotect/PantryProtectorActivity.java
+++ pantryProtector/src/us/food4thought/pantryprotect/PantryProtectorActivity.java
@@ -1,13 +1,94 @@
+/**
+ * HelloAndroid
+ * 
+ * Version 0.0.0
+ *
+ * 09/22/2011
+ */
+
 package us.food4thought.pantryprotect;
 
 import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TabHost;
 
-public class PantryProtectorActivity extends Activity {
+public class PantryProtectorActivity extends TabActivity {
+	
+	public static int expired = 5;
+	
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {  	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Resources res = getResources();
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec spec;
+        Intent intent;
+        
+        intent = new Intent().setClass(this, ScanActivity.class);
+        spec = tabHost.newTabSpec("scan").setIndicator("Scan", res.getDrawable(R.drawable.ic_tab_scan)).setContent(intent);
+        tabHost.addTab(spec);
+        
+        intent = new Intent().setClass(this, InvTabActivity.class);
+        spec = tabHost.newTabSpec("inv").setIndicator("Inventory", res.getDrawable(R.drawable.ic_tab_inv)).setContent(intent);
+        tabHost.addTab(spec);
+        
+        intent = new Intent().setClass(this, GListActivity.class);
+        spec = tabHost.newTabSpec("glist").setIndicator("Grocery List", res.getDrawable(R.drawable.ic_tab_inv)).setContent(intent);
+        tabHost.addTab(spec);
+        
+        intent = new Intent().setClass(this, OptionsActivity.class);
+        spec = tabHost.newTabSpec("opts").setIndicator("Options", res.getDrawable(R.drawable.ic_tab_opts)).setContent(intent);
+        tabHost.addTab(spec);
+        
+        tabHost.setCurrentTab(0);
+        
+    	startService(new Intent(PantryProtectorActivity.this, NotificationService.class));
+    }
+    
+    public static class Controller extends Activity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.main);
+
+            // Watch for button clicks.
+            Button button = (Button)findViewById(R.id.invFrame);
+            button.setOnClickListener(mStartListener);
+            button = (Button)findViewById(R.id.include1);
+            button.setOnClickListener(mStopListener);
+        }
+
+        private OnClickListener mStartListener = new OnClickListener() {
+            public void onClick(View v) {
+                // Make sure the service is started.  It will continue running
+                // until someone calls stopService().  The Intent we use to find
+                // the service explicitly specifies our service component, because
+                // we want it running in our own process and don't want other
+                // applications to replace it.
+                startService(new Intent(Controller.this, NotificationService.class));
+            }
+
+        };
+
+        private OnClickListener mStopListener = new OnClickListener() {
+            public void onClick(View v) {
+                // Cancel a previous call to startService().  Note that the
+                // service will not actually stop at this point if there are
+                // still bound clients.
+                stopService(new Intent(Controller.this, NotificationService.class));
+            }
+
+				
+        };
     }
 }
